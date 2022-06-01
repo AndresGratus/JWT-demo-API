f946386f66bc2bc828c9d9b62843f9ceb3ef0bf2
Andres <andres.castaneda280040@gmail.com>
2022-06-01 12:07:22 -0500

    RepositorioUser

--- src/main/java/com/DEMOJWT/demo/controller/UserController.java
+++ src/main/java/com/DEMOJWT/demo/controller/UserController.java
@@ -3,11 +3,13 @@ package com.DEMOJWT.demo.controller;
 import com.DEMOJWT.demo.dto.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import repositories.RepositorioUser;
 
 import java.util.Date;
 import java.util.List;
@@ -16,6 +18,9 @@ import java.util.stream.Collectors;
 @RestController
 public class UserController {
 
+    @Autowired
+    private RepositorioUser repositorioUser;
+
     @PostMapping("user")
     public User login(@RequestParam("user") String username, @RequestParam("password") String pwd) {
 
@@ -23,6 +28,7 @@ public class UserController {
         User user = new User();
         user.setUser(username);
         user.setToken(token);
+
         return user;
 
     }
